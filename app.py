e047d7182d530ff8b0563704303d9f84b10efc51
Tim Jones <tim.w.j@hotmail.com>
2024-03-27 17:28:30 +0000

    Initial development version

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template
+from werkzeug.exceptions import HTTPException
+
+app=Flask(__name__)
+
+@app.route("/")
+def index():
+  return render_template("index.html")
+
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html", e=e), 404
+
+
+@app.errorhandler(403)
+def forbidden_page(e):
+    return render_template("403.html", e=e), 403
+
+
+@app.errorhandler(410)
+def page_not_exsist(e):
+    return render_template("410.html", e=e), 410
+
+
+@app.errorhandler(Exception)
+def handle_exception(e):
+    # pass through HTTP errors
+    if isinstance(e, HTTPException):
+        return e
+    # non-HTTP exceptions only
+    return render_template("500.html", e=e), 500
+
+
+
+if __name__ == "__main__":
+    app.run(debug=False)
---
+++ static/JS/script.js
@@ -0,0 +1,114 @@
+    /*
+      // 2. This code loads the IFrame Player API code asynchronously.
+      var tag = document.createElement('script');
+
+      tag.src = "https://www.youtube.com/iframe_api";
+      var firstScriptTag = document.getElementsByTagName('script')[0];
+      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+      // 3. This function creates an <iframe> (and YouTube player)
+      //    after the API code downloads.
+      var player;
+      function onYouTubeIframeAPIReady() {
+        player = new YT.Player('player', {
+          height: '390',
+          width: '640',
+          videoId: 'M7lc1UVf-VE',
+          playerVars: {
+            'playsinline': 1
+          },
+          events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+          }
+        });
+      }
+
+     // https://www.youtube.com/channel/UCRbVYPLvBSzEyrlCiRa4fZg
+
+      // 4. The API will call this function when the video player is ready.
+      function onPlayerReady(event) {
+        event.target.playVideo();
+      }
+
+      // 5. The API calls this function when the player's state changes.
+      //    The function indicates that when playing a video (state=1),
+      //    the player should play for six seconds and then stop.
+      var done = false;
+      function onPlayerStateChange(event) {
+        if (event.data == YT.PlayerState.PLAYING && !done) {
+          setTimeout(stopVideo, 6000);
+          done = true;
+        }
+      }
+      function stopVideo() {
+        player.stopVideo();
+      }
+
+/* API code for channel */
+
+/**
+   * Sample JavaScript code for youtube.channels.list
+   * See instructions for running APIs Explorer code samples locally:
+   * https://developers.google.com/explorer-help/code-samples#javascript
+   */
+/*
+function authenticate() {
+  return gapi.auth2.getAuthInstance()
+      .signIn({scope: "https://www.googleapis.com/auth/youtube.readonly"})
+      .then(function() { console.log("Sign-in successful"); },
+            function(err) { console.error("Error signing in", err); });
+}
+function loadClient() {
+  gapi.client.setApiKey("AIzaSyAyg3E8DIK6StxzaSXmzQgPVw6Zv4ojMp4");
+  return gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
+      .then(function() { console.log("GAPI client loaded for API"); },
+            function(err) { console.error("Error loading GAPI client for API", err); });
+}
+// Make sure the client is loaded and sign-in is complete before calling this method.
+function execute() {
+  return gapi.client.youtube.channels.list({
+    "part": [
+      "snippet,contentDetails,statistics"
+    ],
+    "id": [
+      "UCRbVYPLvBSzEyrlCiRa4fZg"
+    ]
+  })
+      .then(function(response) {
+              // Handle the results here (response.result has the parsed body).
+              console.log("Response", response);
+            },
+            function(err) { console.error("Execute error", err); });
+}
+gapi.load("client:auth2", function() {
+  gapi.auth2.init({client_id: "gxtv-408810"});
+});
+*/
+
+(function() {
+  // https://dashboard.emailjs.com/admin/account
+  emailjs.init('OC1B4qLitG4-eOaFz');
+})();
+
+window.onload = function() {
+  document.getElementById('contact-form').addEventListener('submit', function(event) {
+      event.preventDefault();
+      // generate a five digit number for the contact_number variable
+      this.contact_number.value = Math.random() * 100000 | 0;
+      // these IDs from the previous steps
+      emailjs.sendForm("service_7nimshf","contact_form",this)
+          .then(function() {
+              console.log('SUCCESS!');
+              // let name = document.getElementById("user_name");
+              // let email = document.getElementById("user_email");
+              // let message = document.getElementById("message");
+              // name.value =" ";
+              // email.value =" ";
+              // message.value=" ";
+          }, function(error) {
+              console.log('FAILED...', error);
+          });
+          //window.location.replace("{{ url_for('index') }}");
+  });
+}
